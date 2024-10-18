4d5f587ad8de6336adc461a1680401b209303908
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-18 17:26:03 +0430

    Add user relationship to the article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HasAuthor;
+use App\Traits\ModelHelpers;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    use HasAuthor, ModelHelpers;
     //
     const TABLE = 'articles';
     protected $table = self::TABLE;
@@ -16,4 +19,10 @@ class Article extends Model
         'body',
         'author_id',
     ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'author_id', 'id');
+    }
 }
