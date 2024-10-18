92067f9983ec800dfde4237acc5920f0ae1216ad
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-19 00:35:28 +0430

    Create an empty author controller

---
+++ app/Http/Controllers/api/v1/AuthorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function show(User $user)
+    {
+
+    }
+}
