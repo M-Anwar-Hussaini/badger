bd9b2b1f6a5f434f9473652acc7f9f491d5cbadb
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-22 22:53:38 +0430

    Customize the NotFoundHttpException

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,14 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request as HttpRequest;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -23,5 +25,16 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, HttpRequest $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'error' => [
+                        'message' => 'Resource not found',
+                        'type' => 'NotFoundHttpException',
+                        'code' => '4405',
+                        'status_code' => $e->getStatusCode()
+                    ]
+                ]);
+            }
+        });
     })->create();
