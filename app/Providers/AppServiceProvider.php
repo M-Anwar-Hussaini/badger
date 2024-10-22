11a8a4a532cf677f6a76360e4aa0e3eb4fb6e33a
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-22 22:53:02 +0430

    Change route binding using slug or id

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Notifications\ResetPassword;
+use App\Models\Article;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+        Route::bind('article', function (string $value) {
+            return Article::where('id', operator: $value)
+                ->orWhere('slug', $value)
+                ->firstOrFail();
         });
     }
 }
