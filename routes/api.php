b9072b3f9884f8ca8e8b77d0ae336c54bcc90701
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-24 11:47:13 +0430

    Add routes to the api route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,29 @@
 <?php
 
-use App\Http\Controllers\api\v1\ArticleController;
-use App\Http\Controllers\api\v1\AuthorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\api\v1\AuthorController;
+use App\Http\Controllers\api\v1\ArticleController;
+use Illuminate\Http\Response;
 
-Route::prefix('/v1')->group(function () {
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post(uri: '/register', action: [AuthController::class, 'register'])->name('register');
+
+Route::prefix('/v1')->middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/articles', ArticleController::class);
     Route::get('/authors/{user}', [AuthorController::class, 'show'])->name('authors');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json(
+            ['user' => $request->user()]
+            ,
+            Response::HTTP_OK
+        );
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    ;
 });
